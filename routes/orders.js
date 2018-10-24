58a7d6f989cbf1728065923cd34f06c749b3c95f
wfatec <chao125010@163.com>
2018-10-24 11:43:50 +0800

    add Joi for validation

--- routes/orders.js
+++ routes/orders.js
@@ -1,3 +1,5 @@
+const Joi = require('joi');
+
 const GROUP_NAME = 'orders';
 
 module.exports = [
@@ -10,16 +12,34 @@ module.exports = [
         options: {
             tags: ['api', GROUP_NAME],
             description: '创建订单',
+            validate: {
+                payload: {
+                    goodsList: Joi.array().items(
+                        Joi.object().keys({
+                            goods_id: Joi.number().integer(),
+                            count: Joi.number().integer(),
+                        })
+                    )
+                },
+                headers: Joi.object({
+                    authorization: Joi.string().required(),
+                }).unknown(),
+            },
         }
     }, {
         method: 'POST',
         path: `/${GROUP_NAME}/{orderId}/pay`,
         handler: (request, h) => {
-            return;
+            return h;
         },
         options: {
             tags: ['api', GROUP_NAME],
             description: '支付某条订单',
+            validate: {
+                params: {
+                  orderId: Joi.string().required(),
+                },
+            },
         }
     }
 ]
--- routes/shops.js
+++ routes/shops.js
@@ -1,3 +1,5 @@
+const Joi = require('joi');
+
 const GROUP_NAME = 'shops';
 
 module.exports = [
@@ -10,6 +12,14 @@ module.exports = [
         options: {
             tags: ['api', GROUP_NAME],
             description: '获取店铺列表',
+            validate: {
+                query: {
+                    limit: Joi.number().integer().min(1).default(10)
+                        .description('每页的条目数'),
+                    page: Joi.number().integer().min(1).default(1)
+                        .description('页码数'),
+                }
+            },
         }
     }, {
         method: 'GET',
