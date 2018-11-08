9f2e416027be94c96761c1afb256c25603ce31f9
wfatec <chao125010@163.com>
2018-11-08 16:58:27 +0800

    Login authorization and JWT issued

--- plugins/hapi-swagger.js
+++ plugins/hapi-swagger.js
@@ -20,6 +20,7 @@ module.exports = [
                 { name: 'tests', description: '测试相关' },
                 { name: 'shops', description: '店铺、商品相关' },
                 { name: 'orders', description: '订单相关' },
+                { name: 'users', description: '用户相关' },
             ]
         }
     }
